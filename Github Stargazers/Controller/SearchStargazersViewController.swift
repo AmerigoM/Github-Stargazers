9e4c76110b62397255d9ea3d6cf051020331ca7e
AmerigoM <amerigo.mancino@gmail.com>
2020-10-29 14:35:21 +0100

    Networking set up in place; segue set up in place.

--- Github Stargazers/Controller/SearchStargazersViewController.swift
+++ Github Stargazers/Controller/SearchStargazersViewController.swift
@@ -12,50 +12,72 @@ import SwiftyJSON
 class SearchStargazersViewController: UIViewController {
     
     // Constants
-    let GITHUB_URL = "https://api.github.com/repos/AmerigoM/Clima/stargazers"
-    let stargazers = "stargazers"
+    let GITHUB_URL = "https://api.github.com/repos"
     
+    // Outlets
     @IBOutlet weak var ownerLabel: UITextField!
     @IBOutlet weak var repoLabel: UITextField!
+    
+    private var userList = [User]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello")
     }
 
     @IBAction func search(_ sender: UIButton) {
-        if !(ownerLabel.text ?? "").isEmpty {
-            print("Owner not null")
+        if let owner = ownerLabel.text, let repo = repoLabel.text, !owner.isEmpty, !repo.isEmpty {
+            userList = []
+            getGithubData(url: GITHUB_URL, owner: owner, repo: repo)
         } else {
-            // Create new Alert
-            let dialogMessage = UIAlertController(title: "Warning", message: "Input fields cannot be empty!", preferredStyle: .alert)
-            
-            // Create OK button without action handler
-            let ok = UIAlertAction(title: "OK", style: .default)
-            
-            dialogMessage.addAction(ok)
-            self.present(dialogMessage, animated: true, completion: nil)
+            displayAlert(title: "Warning", message: "Input fields cannot be empty.")
         }
-        /*
-        let params : [String : String] = ["owner" : ownerLabel.text!, "repo" : repoLabel.text!, "stargazers" : stargazers]
-        */
     }
     
     
     // MARK: - Networking
     
-    func getGithubData(url: String, parameters: [String : String]) {
+    func getGithubData(url: String, owner: String, repo: String) {
         // http request to the server
-        Alamofire.request(url, method: .get, parameters: parameters).responseJSON { response in
+        let trueUrl = url + "/" + owner + "/" + repo + "/stargazers"
+        Alamofire.request(trueUrl, method: .get).responseJSON { response in
             if response.result.isSuccess {
-                print("Success")
-                print(response)
+                let usersJSON: JSON = JSON(response.result.value!)
+                
+                let jsonArray = usersJSON.array!
+                for item in jsonArray {
+                    let user = User()
+                    user.username = item["login"].stringValue
+                    user.imageLink = item["avatar_url"].stringValue
+                    self.userList.append(user)
+                }
+                
+                self.performSegue(withIdentifier: "segueToList", sender: self)
             } else {
-                print("Failure")
-                print(response)
+                self.displayAlert(title: "Request failure", message: "Please contact the app support.")
             }
         }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueToList" {
+            let destinationVC = segue.destination as! StarredUsersListViewController
+            destinationVC.userList = self.userList
+        }
+    }
+    
+    
+    // MARK: - Support functions
+    
+    func displayAlert(title: String, message: String) {
+        // Create new alert
+        let dialogMessage = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // Create OK button without action handler
+        let ok = UIAlertAction(title: "OK", style: .default)
+        
+        dialogMessage.addAction(ok)
+        self.present(dialogMessage, animated: true, completion: nil)
     }
     
     
---
+++ Github Stargazers/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Github Stargazers
+//
+//  Created by Amerigo Mancino on 29/10/2020.
+//
+
+import Foundation
+
+class User {
+    var username: String = ""
+    var imageLink: String = ""
+}
