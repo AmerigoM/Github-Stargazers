cca63a7f3b7f4588b14ec8891e9d2dc5ab0a827b
AmerigoM <amerigo.mancino@gmail.com>
2020-11-01 13:38:45 +0100

    Added pods to source control.

---
+++ Pods/Target Support Files/Pods-Github Stargazers-Github StargazersUITests/Pods-Github Stargazers-Github StargazersUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Github_Stargazers_Github_StargazersUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Github_Stargazers_Github_StargazersUITests
+@end
